9cf8c1a7473577b3acfaf6b9fcbe3ad27095f55e
Adam Hathcock <adam@hathcock.co.uk>
2013-07-27 08:30:20 +0100

    Moved classes into own files

---
+++ SharpCompress/Crypto/ICipherParameters.cs
@@ -0,0 +1,6 @@
+﻿namespace Org.BouncyCastle.Crypto
+{
+    public interface ICipherParameters
+    {
+    }
+}
---
+++ SharpCompress/Crypto/KeyParameter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Org.BouncyCastle.Crypto.Parameters
+{
+    public class KeyParameter
+        : ICipherParameters
+    {
+        private readonly byte[] key;
+
+        public KeyParameter(
+            byte[] key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+
+            this.key = (byte[])key.Clone();
+        }
+
+        public KeyParameter(
+            byte[] key,
+            int keyOff,
+            int keyLen)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+            if (keyOff < 0 || keyOff > key.Length)
+                throw new ArgumentOutOfRangeException("keyOff");
+            if (keyLen < 0 || (keyOff + keyLen) > key.Length)
+                throw new ArgumentOutOfRangeException("keyLen");
+
+            this.key = new byte[keyLen];
+            Array.Copy(key, keyOff, this.key, 0, keyLen);
+        }
+
+        public byte[] GetKey()
+        {
+            return (byte[])key.Clone();
+        }
+    }
+}
