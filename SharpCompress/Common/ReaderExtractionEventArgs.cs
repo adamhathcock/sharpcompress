7e3f04e669a8fbc05e64fe7bb2cf36267b6f1548
larvata <alzzlnewtype@gmail.com>
2014-12-22 16:34:47 +0800

    add entryExtractionEvent for stream

---
+++ SharpCompress/Common/ReaderExtractionEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SharpCompress.Common
+{
+    public class ReaderExtractionEventArgs<T>:EventArgs
+    {
+        internal ReaderExtractionEventArgs(T entry)
+        {
+            Item = entry;
+        } 
+        public T Item { get; private set; }
+    }
+}
