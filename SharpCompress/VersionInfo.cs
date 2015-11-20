27a4f78712efd9fdb3f4d62d57a2da4d9dca4db0
Adam Hathcock <adam@hathcock.co.uk>
2015-11-20 18:59:43 +0000

    version 0.11.2

--- SharpCompress/VersionInfo.cs
+++ SharpCompress/VersionInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Adam Hathcock")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.10.3.0")]
-[assembly: AssemblyFileVersion("0.10.3.0")]
+[assembly: AssemblyVersion("0.11.2.0")]
+[assembly: AssemblyFileVersion("0.11.2.0")]
