505838a32a7c6dd301f9631059e3a981b71e4ece
Adam Hathcock <adam@hathcock.co.uk>
2016-01-04 21:08:30 +0000

    Fixes from last merge

---
+++ src/SharpCompress/Common/IEntry.Extensions.cs
@@ -0,0 +1,49 @@
+﻿#if !NO_FILE
+using System.IO;
+
+namespace SharpCompress.Common
+{
+    internal static class IEntryExtensions
+    {
+        internal static void PreserveExtractionOptions(this IEntry entry, string destinationFileName,
+                                                        ExtractOptions options)
+        {
+            if (options.HasFlag(ExtractOptions.PreserveFileTime) || options.HasFlag(ExtractOptions.PreserveAttributes))
+            {
+                FileInfo nf = new FileInfo(destinationFileName);
+                if (!nf.Exists)
+                {
+                    return;
+                }
+
+                // update file time to original packed time
+                if (options.HasFlag(ExtractOptions.PreserveFileTime))
+                {
+                    if (entry.CreatedTime.HasValue)
+                    {
+                        nf.CreationTime = entry.CreatedTime.Value;
+                    }
+
+                    if (entry.LastModifiedTime.HasValue)
+                    {
+                        nf.LastWriteTime = entry.LastModifiedTime.Value;
+                    }
+
+                    if (entry.LastAccessedTime.HasValue)
+                    {
+                        nf.LastAccessTime = entry.LastAccessedTime.Value;
+                    }
+                }
+
+                if (options.HasFlag(ExtractOptions.PreserveAttributes))
+                {
+                    if (entry.Attrib.HasValue)
+                    {
+                        nf.Attributes = (FileAttributes)System.Enum.ToObject(typeof(FileAttributes), entry.Attrib.Value);
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
