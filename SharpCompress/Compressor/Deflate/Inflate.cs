ecceec8e1a9771499931f9ff33518174875ce16f
Kenneth Skovhede <kenneth@hexad.dk>
2016-01-22 15:06:55 +0100

    Tabs -> spaces fix

--- SharpCompress/Compressor/Deflate/Inflate.cs
+++ SharpCompress/Compressor/Deflate/Inflate.cs
@@ -895,11 +895,11 @@ namespace SharpCompress.Compressor.Deflate
                                 r = ZlibConstants.Z_OK;
                             else
                             {
-								// Handling missing trailing bit(s)
-								var tmp_tindex = (tree_index + (b & InternalInflateConstants.InflateMask[k]))*3;
-								if (k >= tree[tmp_tindex + 1])
-									break;
-								
+                                // Handling missing trailing bit(s)
+                                var tmp_tindex = (tree_index + (b & InternalInflateConstants.InflateMask[k]))*3;
+                                if (k >= tree[tmp_tindex + 1])
+                                    break;
+                                
                                 blocks.bitb = b;
                                 blocks.bitk = k;
                                 z.AvailableBytesIn = n;
@@ -1003,10 +1003,10 @@ namespace SharpCompress.Compressor.Deflate
                                 r = ZlibConstants.Z_OK;
                             else
                             {
-								// Handling missing trailing bit(s)
-								var tmp_tindex = (tree_index + (b & InternalInflateConstants.InflateMask[k]))*3;
-								if (k >= tree[tmp_tindex + 1])
-									break;
+                                // Handling missing trailing bit(s)
+                                var tmp_tindex = (tree_index + (b & InternalInflateConstants.InflateMask[k]))*3;
+                                if (k >= tree[tmp_tindex + 1])
+                                    break;
 
                                 blocks.bitb = b;
                                 blocks.bitk = k;
