8f02446d04a204145819f5d486148d06e67e4ef4
Natalia Portillo <claunia@claunia.com>
2016-09-30 07:15:13 +0100

    Corrected copyright messages.

--- src/SharpCompress/Compressors/ADC/ADCBase.cs
+++ src/SharpCompress/Compressors/ADC/ADCBase.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Natalia Portillo <claunia@claunia.com>
 //
-// Copyright (c) 2016 © Claunia.com
+// Copyright (c) 2016 Natalia Portillo
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
