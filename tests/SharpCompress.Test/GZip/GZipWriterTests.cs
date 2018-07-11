4cd80e96f3c514383e7921741061f4716cd4b9e4
Matt Kotsenas <matt.kotsenas@gmail.com>
2018-07-11 14:14:16 -0700

    Simplify GZip bad compression test

--- tests/SharpCompress.Test/GZip/GZipWriterTests.cs
+++ tests/SharpCompress.Test/GZip/GZipWriterTests.cs
@@ -42,16 +42,12 @@ namespace SharpCompress.Test.GZip
         public void GZip_Writer_Generic_Bad_Compression()
         {
             Assert.Throws<InvalidFormatException>(() =>
-                                                  {
-                            using (Stream stream = File.OpenWrite(Path.Combine(SCRATCH_FILES_PATH, "Tar.tar.gz")))
-                            using (var writer = WriterFactory.Open(stream, ArchiveType.GZip, CompressionType.BZip2))
-                            {
-                                writer.Write("Tar.tar", Path.Combine(TEST_ARCHIVES_PATH, "Tar.tar"));
-                            }
-                            CompareArchivesByPath(Path.Combine(SCRATCH_FILES_PATH, "Tar.tar.gz"),
-                                Path.Combine(TEST_ARCHIVES_PATH, "Tar.tar.gz"));
-
-                                                  });
+            {
+                using (Stream stream = File.OpenWrite(Path.Combine(SCRATCH_FILES_PATH, "Tar.tar.gz")))
+                using (var writer = WriterFactory.Open(stream, ArchiveType.GZip, CompressionType.BZip2))
+                {
+                }
+            });
         }
     }
 }
