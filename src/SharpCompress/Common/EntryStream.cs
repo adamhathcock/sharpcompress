efae8328a9801e9729f2ec54b9a305c3392c055c
Knut Ørland <ko@zaptec.com>
2018-10-04 13:05:36 +0200

    Don't throw an exception when flushing an EntryStream
    From Microsoft docs: “In a class derived from Stream that doesn't
    support writing, Flush is typically implemented as an empty method to
    ensure full compatibility with other Stream types since it's valid to
    flush a read-only stream.”

--- src/SharpCompress/Common/EntryStream.cs
+++ src/SharpCompress/Common/EntryStream.cs
@@ -47,10 +47,8 @@ namespace SharpCompress.Common
 
         public override bool CanWrite => false;
 
-        public override void Flush()
-        {
-            throw new NotSupportedException();
-        }
+        public override void Flush() {
+		}
 
         public override long Length => _stream.Length;
 
