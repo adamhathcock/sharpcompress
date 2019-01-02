875862c6a5e8567fab4f4593d55889c936cba7f5
Alexei Shcherbakov <>
2019-01-02 21:46:35 +0300

    TryOpen API

--- src/SharpCompress/Readers/ReaderFactory.cs
+++ src/SharpCompress/Readers/ReaderFactory.cs
@@ -30,9 +30,9 @@ namespace SharpCompress.Readers
         {
             stream.CheckNotNull("stream");
             options = options ?? new ReaderOptions()
-                                 {
-                                     LeaveStreamOpen = false
-                                 };
+            {
+                LeaveStreamOpen = false
+            };
             RewindableStream rewindableStream = new RewindableStream(stream);
             rewindableStream.StartRecording();
             if (ZipArchive.IsZipFile(rewindableStream, options.Password))
@@ -102,6 +102,94 @@ namespace SharpCompress.Readers
                 }
             }
             throw new InvalidOperationException("Cannot determine compressed stream type.  Supported Reader Formats: Zip, GZip, BZip2, Tar, Rar, LZip, XZ");
+        }
+
+
+        public static bool TryOpen(Stream stream, ReaderOptions options,ArchiveTypeMask archiveTypes, out IReader reader)
+        {
+            stream.CheckNotNull("stream");
+            options = options ?? new ReaderOptions()
+            {
+                LeaveStreamOpen = false
+            };
+            RewindableStream rewindableStream = new RewindableStream(stream);
+            rewindableStream.StartRecording();
+            if (archiveTypes.HasFlag(ArchiveTypeMask.Zip)&& ZipArchive.IsZipFile(rewindableStream, options.Password))
+            {
+                rewindableStream.Rewind(true);
+                reader = ZipReader.Open(rewindableStream, options);
+                return true;
+            }
+            rewindableStream.Rewind(false);
+            if (archiveTypes.HasFlag(ArchiveTypeMask.GZip) && GZipArchive.IsGZipFile(rewindableStream))
+            {
+                rewindableStream.Rewind(false);
+                GZipStream testStream = new GZipStream(rewindableStream, CompressionMode.Decompress);
+                if (archiveTypes.HasFlag(ArchiveTypeMask.Tar) && TarArchive.IsTarFile(testStream))
+                {
+                    rewindableStream.Rewind(true);
+                    reader = new TarReader(rewindableStream, options, CompressionType.GZip);
+                    return true;
+                }
+                rewindableStream.Rewind(true);
+                reader = GZipReader.Open(rewindableStream, options);
+                return true;
+            }
+
+            rewindableStream.Rewind(false);
+            if (archiveTypes.HasFlag(ArchiveTypeMask.BZip2) && BZip2Stream.IsBZip2(rewindableStream))
+            {
+                rewindableStream.Rewind(false);
+                BZip2Stream testStream = new BZip2Stream(new NonDisposingStream(rewindableStream), CompressionMode.Decompress, false);
+                if (archiveTypes.HasFlag(ArchiveTypeMask.Tar) && TarArchive.IsTarFile(testStream))
+                {
+                    rewindableStream.Rewind(true);
+                    reader = new TarReader(rewindableStream, options, CompressionType.BZip2);
+                    return true;
+                }
+            }
+
+            rewindableStream.Rewind(false);
+            if (archiveTypes.HasFlag(ArchiveTypeMask.LZip) && LZipStream.IsLZipFile(rewindableStream))
+            {
+                rewindableStream.Rewind(false);
+                LZipStream testStream = new LZipStream(new NonDisposingStream(rewindableStream), CompressionMode.Decompress);
+                if (archiveTypes.HasFlag(ArchiveTypeMask.Tar) && TarArchive.IsTarFile(testStream))
+                {
+                    rewindableStream.Rewind(true);
+                    reader = new TarReader(rewindableStream, options, CompressionType.LZip);
+                    return true;
+                }
+            }
+            rewindableStream.Rewind(false);
+            if (archiveTypes.HasFlag(ArchiveTypeMask.Rar) && RarArchive.IsRarFile(rewindableStream, options))
+            {
+                rewindableStream.Rewind(true);
+                reader = RarReader.Open(rewindableStream, options);
+                return true;
+            }
+
+            rewindableStream.Rewind(false);
+            if (archiveTypes.HasFlag(ArchiveTypeMask.Tar) && TarArchive.IsTarFile(rewindableStream))
+            {
+                rewindableStream.Rewind(true);
+                reader = TarReader.Open(rewindableStream, options);
+                return true;
+            }
+            rewindableStream.Rewind(false);
+            if (XZStream.IsXZStream(rewindableStream))
+            {
+                rewindableStream.Rewind(true);
+                XZStream testStream = new XZStream(rewindableStream);
+                if (archiveTypes.HasFlag(ArchiveTypeMask.Tar) && TarArchive.IsTarFile(testStream))
+                {
+                    rewindableStream.Rewind(true);
+                    reader = new TarReader(rewindableStream, options, CompressionType.Xz);
+                    return true;
+                }
+            }
+            reader = null;
+            return false;
         }
     }
 }
