0f06c3d934a55470761e7d658f80dc7eb0731635
Adam Hathcock <adam@hathcock.co.uk>
2021-01-13 14:40:36 +0000

    Fix rewindable stream to expose length

--- tests/SharpCompress.Test/GZip/GZipReaderTests.cs
+++ tests/SharpCompress.Test/GZip/GZipReaderTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using SharpCompress.Common;
+using SharpCompress.IO;
 using Xunit;
 
 namespace SharpCompress.Test.GZip
@@ -21,11 +22,15 @@ namespace SharpCompress.Test.GZip
         [Fact]
         public void GZip_Reader_Generic2()
         {
+            //read only as GZip itme
             using (Stream stream = File.OpenRead(Path.Combine(TEST_ARCHIVES_PATH, "Tar.tar.gz")))
-            using (SharpCompress.Readers.IReader reader = SharpCompress.Readers.GZip.GZipReader.Open(stream))
+            using (var reader = SharpCompress.Readers.GZip.GZipReader.Open(new RewindableStream(stream)))
             {
                 while (reader.MoveToNextEntry()) // Crash here
-                { }
+                {
+                    Assert.NotEqual(0, reader.Entry.Size);
+                    Assert.NotEqual(0, reader.Entry.Crc);
+                }
             }
         }
     }
