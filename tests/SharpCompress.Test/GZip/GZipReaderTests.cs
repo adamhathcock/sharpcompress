9d5cb8d1192e2af7b4e9da4ae955e5451973139f
Adam Hathcock <adam@hathcock.co.uk>
2021-01-13 10:42:59 +0000

    Add GZip Reader tests

---
+++ tests/SharpCompress.Test/GZip/GZipReaderTests.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using SharpCompress.Common;
+using Xunit;
+
+namespace SharpCompress.Test.GZip
+{
+    public class GZipReaderTests : ReaderTests
+    {
+        public GZipReaderTests()
+        {
+            UseExtensionInsteadOfNameToVerify = true;
+        }
+
+        [Fact]
+        public void GZip_Reader_Generic()
+        {
+            Read("Tar.tar.gz", CompressionType.GZip);
+        }
+        
+        
+        [Fact]
+        public void GZip_Reader_Generic2()
+        {
+            using (Stream stream = File.OpenRead(Path.Combine(TEST_ARCHIVES_PATH, "Tar.tar.gz")))
+            using (SharpCompress.Readers.IReader reader = SharpCompress.Readers.GZip.GZipReader.Open(stream))
+            {
+                while (reader.MoveToNextEntry()) // Crash here
+                { }
+            }
+        }
+    }
+}
