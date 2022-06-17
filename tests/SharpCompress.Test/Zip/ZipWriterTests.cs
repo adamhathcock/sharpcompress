a00075ee0d3a712c4aa4cdb44c7a4f19eb7f8b6a
Lars Vahlenberg <lars.vahlenberg@gmail.com>
2022-06-17 15:07:07 +0200

    Wrong flags set, we do not expose this in the interface

--- tests/SharpCompress.Test/Zip/ZipWriterTests.cs
+++ tests/SharpCompress.Test/Zip/ZipWriterTests.cs
@@ -2,9 +2,6 @@ using System.Text;
 
 using SharpCompress.Common;
 using Xunit;
-using System.IO;
-using SharpCompress.Writers.Zip;
-using SharpCompress.Compressors.Deflate;
 
 namespace SharpCompress.Test.Zip
 {
@@ -51,24 +48,6 @@ namespace SharpCompress.Test.Zip
         public void Zip_Rar_Write()
         {
             Assert.Throws<InvalidFormatException>(() => Write(CompressionType.Rar, "Zip.ppmd.noEmptyDirs.zip", "Zip.ppmd.noEmptyDirs.zip"));
-        }
-
-        [Fact]
-        public void Zip_Write_MemoryStream()
-        {
-            var ms = new MemoryStream();
-            var zw = new ZipWriter(ms, new ZipWriterOptions(compressionType: CompressionType.Deflate ) { DeflateCompressionLevel = CompressionLevel.None } );
-            var payload = new string('\n', 100000);
-            using (var stream = zw.WriteToStream("test.txt", new ZipWriterEntryOptions()))
-                using (var streamWriter = new StreamWriter(stream: stream))
-            {
-                streamWriter.Write(payload);
-            }
-
-            using( var file = new FileStream("d:\\projects\\test.zip", FileMode.Create, FileAccess.Write))
-            {
-                ms.WriteTo(file);
-            }
         }
     }
 }
