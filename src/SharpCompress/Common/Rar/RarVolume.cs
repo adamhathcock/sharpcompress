c37ea5c8905eea9b7379a33a1075c9031226c1b3
IngBertolini <luca.bertolini.1994@gmail.com>
2022-09-29 18:14:56 +0200

    Forgot to consider the zero termination

--- src/SharpCompress/Common/Rar/RarVolume.cs
+++ src/SharpCompress/Common/Rar/RarVolume.cs
@@ -65,7 +65,7 @@ namespace SharpCompress.Common.Rar
 							    var part = CreateFilePart(lastMarkHeader!, fh);
 							    var buffer = new byte[fh.CompressedSize];
 							    part.GetCompressedStream().Read(buffer, 0, buffer.Length);
-							    Comment = System.Text.Encoding.UTF8.GetString(buffer);
+							    Comment = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length - 1);
 						    }
 					    }
 					    break;        
