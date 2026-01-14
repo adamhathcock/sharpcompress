227fec66ad7403a1bf4650f552d1cbac3cef6280
Adam Hathcock <adam@hathcock.uk>
2026-01-14 16:49:34 +0000

    more pooling

--- src/SharpCompress/IO/BufferedSubStream.cs
+++ src/SharpCompress/IO/BufferedSubStream.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Buffers;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,13 +29,16 @@ internal class BufferedSubStream : SharpCompressStream, IStreamStack
 #if DEBUG_STREAMS
         this.DebugDispose(typeof(BufferedSubStream));
 #endif
-        if (disposing) { }
+        if (disposing)
+        {
+            ArrayPool<byte>.Shared.Return(_cache);
+        }
         base.Dispose(disposing);
     }
 
     private int _cacheOffset;
     private int _cacheLength;
-    private readonly byte[] _cache = new byte[32 << 10];
+    private readonly byte[] _cache =  ArrayPool<byte>.Shared.Rent(32 << 10);
     private long origin;
 
     private long BytesLeftToRead { get; set; }
--- src/SharpCompress/IO/SharpCompressStream.cs
+++ src/SharpCompress/IO/SharpCompressStream.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Buffers;
-using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
