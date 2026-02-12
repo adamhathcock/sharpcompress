ae0b4f5c4cfe5ff6cea3b7b3e6b128eb8b22f6ba
Adam Hathcock <adam@hathcock.uk>
2026-02-12 14:31:13 +0000

    forgot some files

--- tests/SharpCompress.Performance/Benchmarks/ZipBenchmarks.cs
+++ tests/SharpCompress.Performance/Benchmarks/ZipBenchmarks.cs
@@ -7,6 +7,7 @@ using SharpCompress.Archives.Zip;
 using SharpCompress.Common;
 using SharpCompress.Compressors;
 using SharpCompress.Providers;
+using SharpCompress.Providers.System;
 using SharpCompress.Readers;
 using SharpCompress.Writers;
 
